3da4747d4a09697555d425bcd051eb5c768ce27a
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-18 17:21:11 -0800

    fix enableCors in main ts'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
-
-  const corsOrigin =
-    process.env.NODE_ENV === 'production'
-      ? process.env.FRONTEND_PRODUCTION_URL
-      : process.env.FRONTEND_LOCALHOST;
 
   app.enableCors({
-    origin: corsOrigin,
+    origin: [
+      process.env.FRONTEND_PRODUCTION_URL,
+      process.env.FRONTEND_LOCALHOST,
+    ],
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
   });
 
