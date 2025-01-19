3c972797b2e9fc7c5bc68eda24e4f69d5cbfd6bf
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-19 11:51:58 -0800

    update backend main.ts for vercel deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+let app: INestApplication<any>;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  if (!app) {
+    app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      process.env.FRONTEND_PRODUCTION_URL,
-      process.env.FRONTEND_LOCALHOST,
-    ],
-    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-  });
+    app.enableCors({
+      origin: [
+        process.env.FRONTEND_PRODUCTION_URL,
+        process.env.FRONTEND_LOCALHOST,
+      ],
+      methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    });
 
-  if (process.env.NODE_ENV !== 'production') {
-    await app.listen(3000);
-  } else {
     await app.init();
   }
+  return app;
 }
-bootstrap();
+
+export default async function handler(req, res) {
+  const app = await bootstrap();
+  const httpAdapter = app.getHttpAdapter();
+  return httpAdapter.getInstance()(req, res);
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then(app => {
+    app.listen(3000);
+  });
+}
