1cb04d06581a10785f16606cfd7229eafcce5d94
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-18 21:15:12 -0800

    trigger build

--- frontend/src/constants/featuredCharities.ts
+++ frontend/src/constants/featuredCharities.ts
@@ -1,37 +1,37 @@
 import Charity from "../interfaces/Charity";
 
 export const featuredCharities: Charity[] = [
-    {
-      _id: '',
-      every_id: "364887167",
-      every_slug: "food-for-life-global-americas-inc",
-      name: "Food For Life Global - Americas Inc",
-      description:
-        "The world's largest vegan food relief network with over 260 charity projects in 60 countries",
-      image_url:
-        "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_3840,q_80/profile_pics/bqpnyxim0yk8cuqw9s6a",
-      website: "https://ffl.org/",
-    },
-    {
-      _id: '',
-      every_id: "810840578",
-      every_slug: "gfi",
-      name: "The Good Food Institute",
-      description:
-        "Developing the roadmap for a sustainable, secure, and just global protein supply 🧫🌱",
-      image_url:
-        "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_3840,q_80/food_s7jfu8",
-      website: "http://www.gfi.org",
-    },
-    {
-      _id: '',
-      every_id: "510292919",
-      every_slug: "farm-sanctuary",
-      name: "Farm Sanctuary",
-      description:
-        "Farm Sanctuary fights the disastrous effects of animal agriculture on animals, the environment, social justice, and public health through rescue, education, and advocacy.",
-      image_url:
-        "https://assets.farmsanctuary.org/content/uploads/2024/12/06092318/2024_09_13_FSAC_Grace_and_Jodean_Pigs_DM_4638-scaled-e1733495282772-1600x911.jpg",
-      website: "https://farmsanctuary.org/",
-    },
-  ];
+  {
+    _id: "",
+    every_id: "364887167",
+    every_slug: "food-for-life-global-americas-inc",
+    name: "Food For Life Global - Americas Inc",
+    description:
+      "The world's largest vegan food relief network with over 260 charity projects in 60 countries",
+    image_url:
+      "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_3840,q_80/profile_pics/bqpnyxim0yk8cuqw9s6a",
+    website: "https://ffl.org/",
+  },
+  {
+    _id: "",
+    every_id: "810840578",
+    every_slug: "gfi",
+    name: "The Good Food Institute",
+    description:
+      "Developing the roadmap for a sustainable, secure, and just global protein supply 🧫🌱",
+    image_url:
+      "https://res.cloudinary.com/everydotorg/image/upload/f_auto,c_limit,w_3840,q_80/food_s7jfu8",
+    website: "http://www.gfi.org",
+  },
+  {
+    _id: "",
+    every_id: "510292919",
+    every_slug: "farm-sanctuary",
+    name: "Farm Sanctuary",
+    description:
+      "Farm Sanctuary fights the disastrous effects of animal agriculture on animals, the environment, social justice, and public health through rescue, education, and advocacy.",
+    image_url:
+      "https://assets.farmsanctuary.org/content/uploads/2024/12/06092318/2024_09_13_FSAC_Grace_and_Jodean_Pigs_DM_4638-scaled-e1733495282772-1600x911.jpg",
+    website: "https://farmsanctuary.org/",
+  },
+];
