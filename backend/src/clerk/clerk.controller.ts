6dd188e0f7d78e940e4e731645dd31f40749ee9f
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-19 13:30:41 -0800

    trigger build

--- backend/src/clerk/clerk.controller.ts
+++ backend/src/clerk/clerk.controller.ts
@@ -14,9 +14,11 @@ export class ClerkController {
   constructor(private readonly clerkService: ClerkService) {}
 
   @Get(':clerkUserId')
-  async getUserName(@Param('clerkUserId') clerkUserId: string): Promise<string> {
+  async getUserName(
+    @Param('clerkUserId') clerkUserId: string,
+  ): Promise<string> {
     try {
-      return  await this.clerkService.getUserName(clerkUserId);
+      return await this.clerkService.getUserName(clerkUserId);
     } catch (error) {
       this.logger.error('User name not available', error.stack);
       throw new NotFoundException('User name not available');
