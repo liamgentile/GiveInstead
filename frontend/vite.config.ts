f424e7dad065c3b67123231f19c00f4c3285925f
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-19 16:42:51 -0800

    try old vite config'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: 'frontend/dist',  
   },
-  base: './' 
 })
