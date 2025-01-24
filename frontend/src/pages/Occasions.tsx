0b6c5b8b826c4cba259f77b15313c4e6401bde12
Liam Gentile <liamanthonygentile@gmail.com>
2025-01-23 16:56:47 -0800

    minor styling fixes

--- frontend/src/pages/Occasions.tsx
+++ frontend/src/pages/Occasions.tsx
@@ -200,7 +200,7 @@ export default function Occasions() {
                         MozAppearance: "none",
                       }}
                     />
-                    <div className="hidden sm:absolute top-3 right-4">
+                    <div className="absolute sm:hidden top-3 right-4">
                       <Calendar className="w-5 h-5 text-gray-800" />
                     </div>
                   </div>
@@ -225,7 +225,7 @@ export default function Occasions() {
                         MozAppearance: "none",
                       }}
                     />
-                    <div className="hidden sm:absolute top-3 right-4">
+                    <div className="absolute sm:hidden top-3 right-4">
                       <Calendar className="w-5 h-5 text-gray-800" />
                     </div>
                   </div>
@@ -391,6 +391,7 @@ export default function Occasions() {
                     setSearchTerm("");
                     setSelectedCharities([]);
                     form.reset();
+                    window.scrollTo(0, 0);
                   }}
                   className="px-6 py-2 text-gray-700 border border-gray-300 rounded-full hover:bg-gray-50"
                 >
